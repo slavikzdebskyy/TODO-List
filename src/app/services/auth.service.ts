6830aa92e0f7544e8dcb2e64487e7650635e0d8e
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2019-11-05 23:51:07 +0200

    [implement-auth-service]  - implemented auth.service.ts

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
+
+import { User } from '../shared/interfaces';
+import { StorageService } from '.';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly MOCK_USER_DATA: User = {
+    name: 'Vasil',
+    lastName: 'Alibabaevich',
+    email: 'test@domain.url',
+    age: 30,
+    id: '30-vasil-alibabaevich',
+  }
+  private readonly MOCK_USER_PASSWORD: string = 'qwerty123';
+
+  constructor(
+    private storageService: StorageService,
+    private router: Router,
+    ) { }
+
+  public login(email: string, password: string): Observable<User> {
+    if (this.MOCK_USER_DATA.email === email && this.MOCK_USER_PASSWORD === password) {
+      return new Observable<User>((observer: Subscriber<User>) => {
+        observer.next(this.MOCK_USER_DATA);
+      });
+    }
+    return new Observable<User>((observer: Subscriber<User>) => {
+      observer.error({msg: 'Wrong credentials'});
+    });
+  }
+
+  public logout(): void {
+    this.storageService.removeUser();
+    this.router.navigate(['']);
+  }
+
+}
